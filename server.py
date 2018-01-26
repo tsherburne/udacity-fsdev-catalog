de2f146f83bc334b81f6459c75cabb14c61bcea6
Tim Sherburne <sherburne.tim@gmail.com>
2018-01-26 16:38:56 +0000

    PEP8 cleanup

--- server.py
+++ server.py
@@ -25,63 +25,71 @@ SCOPES = ['openid']
 
 app = Flask(__name__)
 
+
+# JSON api endpoint
 @app.route('/api/item/<int:item_id>/JSON')
 def apiItemJSON(item_id):
     try:
         item = session.query(Item).filter_by(id=item_id).one()
         return jsonify(Item=item.serialize)
-    except:
+    except DatabaseError:
         return("Item not found!")
-        
+
+
 @app.route('/authorize')
 def authorize():
-    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    # Create flow instance to manage the OAuth 2.0
+    # Authorization Grant Flow steps.
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE, scopes=SCOPES)
-    
+
     flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    
+
     authorization_url, state = flow.authorization_url(
         # Enable offline access so that you can refresh an access token without
-        # re-prompting the user for permission. Recommended for web server apps.
+        # re-prompting the user for permission.
+        # Recommended for web server apps.
         access_type='offline',
         # Enable incremental authorization. Recommended as a best practice.
         include_granted_scopes='true')
-    
+
     # Store the state so the callback can verify the auth server response.
     flask.session['state'] = state
-    
+
     return flask.redirect(authorization_url)
+
 
 @app.route('/oauth2callback')
 def oauth2callback():
     # Specify the state when creating the flow in the callback so that it can
     # verified in the authorization server response.
     state = flask.session['state']
-    
+
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
       CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
     flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
-    
+
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
     authorization_response = flask.request.url
     flow.fetch_token(authorization_response=authorization_response)
-    
+
     # Store credentials in the session.
     credentials = flow.credentials
     flask.session['credentials'] = credentials_to_dict(credentials)
 
     flash('User Logged In')
-    
+
     return flask.redirect(flask.url_for('routeCatalog'))
 
+
 def credentials_to_dict(credentials):
-  return {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
 
 # Upon revoke credential and clear session credentials
 @app.route('/clear')
@@ -89,12 +97,12 @@ def clear_credentials():
     if 'credentials' in flask.session:
         credentials = google.oauth2.credentials.Credentials(
                                 **flask.session['credentials'])
-    
-        requests.post('https://accounts.google.com/o/oauth2/revoke',
-            params={'token': credentials.token},
-            headers = {'content-type': 'application/x-www-form-urlencoded'})
 
-    
+        requests.post('https://accounts.google.com/o/oauth2/revoke',
+                      params={'token': credentials.token},
+                      headers={'content-type':
+                               'application/x-www-form-urlencoded'})
+
         del flask.session['credentials']
 
     flash('User Logged Out')
@@ -118,11 +126,12 @@ def updateItem(item_id):
             session.commit()
             flash('Item Updated')
             return redirect(url_for('editItem', mode="e", item_id=item.id))
-        except:
+        except DatabaseError:
             flash('Item not found')
             return redirect(url_for('routeCatalog'))
     else:
         return redirect(url_for('routeCatalog'))
+
 
 # Delete Item from Database
 @app.route('/deleteItem/<int:item_id>', methods=['POST'])
@@ -133,9 +142,10 @@ def deleteItem(item_id):
             session.delete(item)
             session.commit()
             flash('Item deleted')
-        except:
+        except DatabaseError:
             flash('Item not found')
     return redirect(url_for('routeCatalog'))
+
 
 # Create Item in Database
 @app.route('/createItem', methods=['POST'])
@@ -144,15 +154,15 @@ def createItem():
 
         try:
             print request.form
-            
-            item = Item(name=request.form['name'], 
-                        description=request.form['description'], 
+
+            item = Item(name=request.form['name'],
+                        description=request.form['description'],
                         category_id=request.form['category'])
-                        
+
             session.add(item)
             session.commit()
             flash('Item Created')
-        except:
+        except DatabaseError:
             flash('Create failed')
 
     return redirect(url_for('routeCatalog'))
@@ -167,48 +177,49 @@ def editItem(mode, item_id):
     if mode == "e":
         try:
             item = session.query(Item).filter_by(id=item_id).one()
-        except:
+        except DatabaseError:
             return redirect(url_for('routeCatalog'))
     else:
         item = None
-        
+
     return render_template('item.html', mode=mode, item=item)
 
 
 # Show catalog and item list for selected category
 @app.route('/catalog/<int:category_id>/')
 def showCatalog(category_id):
-      
+
     sel_cat = session.query(Category).filter_by(id=category_id).one_or_none()
-  
+
     if sel_cat is not None:
-        login_session[ 'sel_cat'] = sel_cat.name
+        login_session['sel_cat'] = sel_cat.name
     else:
         if 'sel_cat' in login_session:
-            del login_session[ 'sel_cat']
+            del login_session['sel_cat']
         return redirect(url_for('routeCatalog'))
-        
+
     categories = session.query(Category).order_by(asc(Category.name))
     # Select Items for "selected" Category
     items = session.query(Item).filter_by(category_id=category_id).\
-                order_by(asc(Item.name)).all()
+        order_by(asc(Item.name)).all()
 
     return render_template('catalog.html', categories=categories, items=items)
-    
+
+
 # Redirect to catalog, showing item list for 'first' catalog entry
 @app.route('/')
 def routeCatalog():
     category = session.query(Category).order_by(asc(Category.name)).first()
     category_id = category.id
     return redirect(url_for('showCatalog', category_id=category_id))
-    
+
+
 if __name__ == '__main__':
     # When running locally, disable OAuthlib's HTTPs verification.
     # ACTION ITEM for developers:
     #     When running in production *do not* leave this option enabled.
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    
+
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
-    
